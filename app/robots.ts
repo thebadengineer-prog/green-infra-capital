087cdab5e27948b722ba220bad8ef3be706333f9
thebadengineer-prog <thebadengineer@gmail.com>
2026-07-28 09:35:07 +0200

    Create robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,17 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = "https://greeninfracapital.in";
+
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/api/", "/_next/", "/admin/"],
+      },
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+    host: baseUrl,
+  };
+}
